762e202fc8d6e1f6ae35149f3b4faaa11b91e636
Jan Krupa <jan.krupa@vsb.cz>
2020-05-19 14:20:35 +0200

    Updates

---
+++ precache-manifest.5b8c6726fabc378c08895ad5007550c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "785da89e7c9823fd7964f874baed64ba",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "600660cb72b5b04576e7",
+    "url": "/monsters-rolodex/static/css/main.83a5cb62.chunk.css"
+  },
+  {
+    "revision": "147f1327e7798733efc4",
+    "url": "/monsters-rolodex/static/js/2.d562cfe4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.d562cfe4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "600660cb72b5b04576e7",
+    "url": "/monsters-rolodex/static/js/main.b41d0761.chunk.js"
+  },
+  {
+    "revision": "83fef09b6b4b91e8e383",
+    "url": "/monsters-rolodex/static/js/runtime-main.6036d2a0.js"
+  }
+]);
